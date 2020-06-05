a613697aeba7f25724abe14cdccf28d98a0430a4
matsu <44367201+pinecode1@users.noreply.github.com>
2020-06-05 23:28:29 +0900

    Add:association and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :diaries
+  has_many :likes
+
+  validates :name, {presence: true, length: { maximum: 10 }}
+  validates :email, {presene: true, uniqueness: true}
 end
