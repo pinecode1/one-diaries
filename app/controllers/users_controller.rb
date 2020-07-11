f6aa4ab7d2a0ec65b720449a158bc53401ff9fbb
matsu <44367201+pinecode1@users.noreply.github.com>
2020-07-11 23:57:17 +0900

    Add:Users-show-action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+  def show
+    user = User.find(params[:id])
+    @name = user.name
+    @diaries = user.diaries.page(params[:page]).per(5).order("created_at DESC")
+  end
 end
